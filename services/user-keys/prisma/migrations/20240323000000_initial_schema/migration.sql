a88ed7191607056008c4877e6be0fa411a503821
Chris Jensen <chris@speakeasy.local>
2025-04-10 13:32:55 +1200

    Update user-keys schema

--- services/user-keys/prisma/migrations/20240323000000_initial_schema/migration.sql
+++ services/user-keys/prisma/migrations/20240323000000_initial_schema/migration.sql
@@ -1,13 +1,13 @@
 -- CreateTable
 CREATE TABLE "user_keys" (
 	"id" TEXT NOT NULL,
-	"userId" TEXT NOT NULL,
+	"authorDid" TEXT NOT NULL,
 	"publicKey" TEXT NOT NULL,
 	"privateKey" TEXT NOT NULL,
 	"createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	"updatedAt" TIMESTAMP(3) NOT NULL,
+	"deletedAt" TIMESTAMP(3),
 	CONSTRAINT "user_keys_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE INDEX "user_keys_userId_idx" ON "user_keys"("userId");
+CREATE INDEX "user_keys_userId_idx" ON "user_keys"("authorDid");
