f3d628259cc71681244e86a5ea8f19ba2a4a5422
Chris Jensen <chris@speakeasy.local>
2025-04-19 17:17:14 +1200

    Fix implementation of session change worker

--- services/private-sessions/src/worker.ts
+++ services/private-sessions/src/worker.ts
@@ -6,6 +6,7 @@ import {
   encryptSessionKey,
   decryptSessionKey,
 } from '@speakeasy-services/crypto';
+import logger from './utils/logger.js';
 
 interface AddRecipientToSessionJob {
   authorDid: string;
@@ -20,71 +21,94 @@ const worker = new Worker({ name: 'trusted-users-worker' });
 const queue = Queue.getInstance();
 const prisma = new PrismaClient();
 
+// Add a new recipient to 30 days prior
+const WINDOW_FOR_NEW_TRUSTED_USER = 30 * 24 * 60 * 60 * 1000;
+
 /**
- * When a new recipient is added, ask the private-sessions service to add them to the session.
+ * When a new recipient is added, add them to prior sessions.
  */
 queue.work<AddRecipientToSessionJob>(
   JOB_NAMES.ADD_RECIPIENT_TO_SESSION,
   async (job) => {
     const { authorDid, recipientDid } = job.data;
 
-    const session = await prisma.session.findFirst({
-      where: { authorDid, revokedAt: null },
-      select: { id: true },
+    const sessions = await prisma.session.findMany({
+      where: {
+        authorDid,
+        createdAt: { gt: new Date(Date.now() - WINDOW_FOR_NEW_TRUSTED_USER) },
+      },
+      include: {
+        sessionKeys: {
+          where: {
+            recipientDid: authorDid,
+          },
+        },
+      },
     });
+    const sessionsWithKeys = sessions.filter(
+      (session) => session.sessionKeys.length > 0,
+    );
 
-    // If there's no active session, we don't need add the recipient
-    // they will be added when the next session is created
-    if (!session) return;
-
-    const [sessionKey, authorPrivateKeyBody, recipientPublicKeyBody] =
-      await Promise.all([
-        prisma.sessionKey.findFirst({
-          where: { sessionId: session.id, recipientDid: authorDid },
-          select: { encryptedDek: true },
-        }),
-        speakeasyApiRequest(
-          {
-            method: 'GET',
-            path: 'social.spkeasy.keys.getPrivateKey',
-            fromService: 'private-sessions',
-            toService: 'user-keys',
-          },
-          { did: authorDid },
-        ),
-        speakeasyApiRequest(
-          {
-            method: 'GET',
-            path: 'social.spkeasy.keys.getPublicKey',
-            fromService: 'private-sessions',
-            toService: 'user-keys',
-          },
-          { did: recipientDid },
-        ),
-      ]);
-
-    if (!sessionKey) {
-      throw new Error('Session key not found');
+    if (sessionsWithKeys.length === 0) {
+      logger.log('error', `No sessions found for author ${authorDid}`);
+      return;
     }
 
-    // Decrypt session DEK using author private key
-    const decryptedDek = decryptSessionKey(
-      sessionKey.encryptedDek.toString(),
-      authorPrivateKeyBody.privateKey,
+    if (sessions.length > sessionsWithKeys.length) {
+      logger.error(
+        `Some sessions for ${authorDid} do not have author session keys (${sessions.length} sessions, ${sessionsWithKeys.length})`,
+      );
+    }
+
+    if (!sessionsWithKeys.length) {
+      return;
+    }
+
+    const [authorPrivateKeyBody, recipientPublicKeyBody] = await Promise.all([
+      speakeasyApiRequest(
+        {
+          method: 'GET',
+          path: 'social.spkeasy.keys.getPrivateKey',
+          fromService: 'private-sessions',
+          toService: 'user-keys',
+        },
+        { did: authorDid },
+      ),
+      speakeasyApiRequest(
+        {
+          method: 'GET',
+          path: 'social.spkeasy.keys.getPublicKey',
+          fromService: 'private-sessions',
+          toService: 'user-keys',
+        },
+        { did: recipientDid },
+      ),
+    ]);
+
+    const newSessionKeys = await Promise.all(
+      sessionsWithKeys.map(async (session) => {
+        // Decrypt session DEK using author private key
+        const decryptedDek = await decryptSessionKey(
+          session.sessionKeys[0].encryptedDek.toString(),
+          authorPrivateKeyBody.privateKey,
+        );
+
+        // Encrypt session DEK using recipient public key
+        const encryptedDek = await encryptSessionKey(
+          decryptedDek,
+          recipientPublicKeyBody.publicKey,
+        );
+
+        return {
+          sessionId: session.id,
+          recipientDid,
+          encryptedDek: Buffer.from(encryptedDek),
+        };
+      }),
     );
 
-    // Encrypt session DEK using recipient public key
-    const encryptedDek = encryptSessionKey(
-      decryptedDek,
-      recipientPublicKeyBody.publicKey,
-    );
-
-    await prisma.sessionKey.create({
-      data: {
-        sessionId: session.id,
-        recipientDid,
-        encryptedDek: Buffer.from(encryptedDek),
-      },
+    await prisma.sessionKey.createMany({
+      data: newSessionKeys,
     });
   },
 );
@@ -95,17 +119,10 @@ queue.work<AddRecipientToSessionJob>(
 queue.work<RotateSessionJob>(JOB_NAMES.REVOKE_SESSION, async (job) => {
   const { authorDid } = job.data;
 
-  await speakeasyApiRequest(
-    {
-      method: 'POST',
-      path: 'social.spkeasy.privateSession.Session',
-      fromService: 'trusted-users',
-      toService: 'private-sessions',
-    },
-    {
-      authorDid,
-    },
-  );
+  await prisma.session.updateMany({
+    where: { authorDid, revokedAt: null, expiresAt: { gt: new Date() } },
+    data: { revokedAt: new Date() },
+  });
 });
 
 worker.start().catch((error: Error) => {
