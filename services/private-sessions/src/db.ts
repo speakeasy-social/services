200c7c974bb51521b4c4c07a8424c5f60a7a2f31
Chris Jensen <chris@speakeasy.local>
2025-05-02 14:15:40 +1200

    Support debug option in development for queries

--- services/private-sessions/src/db.ts
+++ services/private-sessions/src/db.ts
@@ -1,6 +1,24 @@
-import { PrismaClient } from './generated/prisma-client/index.js';
+import { Prisma, PrismaClient } from './generated/prisma-client/index.js';
 
-const prisma = new PrismaClient();
+const options: Prisma.PrismaClientOptions = {};
+
+if (process.env.NODE_ENV === 'development' && process.env.PRISMA_LOG) {
+  options.log = [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+  ];
+}
+
+const prisma = new PrismaClient(options);
+
+if (options.log) {
+  (prisma.$on as any)('query', (e: Prisma.QueryEvent) => {
+    console.log('Query: ' + e.query);
+    console.log('Duration: ' + e.duration + 'ms');
+  });
+}
 
 export function getPrismaClient() {
   return prisma;
