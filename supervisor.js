61628134c5008d299157f0ab4ba056e3d71613ef
Chris Jensen <chris@speakeasy.local>
2025-05-02 16:30:58 +1200

    Revert "Fix worker logging"
    This reverts commit a57bf5f3e6349d9611c0bf4f31be05f77a88cb6c.

--- supervisor.js
+++ supervisor.js
@@ -11,17 +11,13 @@ if (cluster.isMaster) {
 
   // Fork processes
   for (let i = 0; i < numProcesses; i++) {
-    const process = cluster.fork();
-    process.process.stdout.pipe(process.stdout);
-    process.process.stderr.pipe(process.stderr);
+    cluster.fork();
   }
 
   cluster.on('exit', (process, code, signal) => {
     console.log(`Process ${process.process.pid} died with code: ${code}, and signal: ${signal}`);
     console.log('Starting a new process');
-    const newProcess = cluster.fork();
-    newProcess.process.stdout.pipe(process.stdout);
-    newProcess.process.stderr.pipe(process.stderr);
+    cluster.fork();
   });
 } else {
   const serviceName = process.env.SERVICE_NAME;
