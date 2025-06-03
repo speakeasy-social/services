9f63308db99302db649392c22b78b2b74499eca6
Chris Jensen <chris@speakeasy.local>
2025-06-03 22:11:33 +1000

    WIP private profiles service

---
+++ services/private-profiles/src/services/profile.service.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '../generated/prisma-client/index.js';
+import { NotFoundError, ValidationError } from '@speakeasy-services/common';
+import { getPrismaClient } from '../db.js';
+
+const prisma = getPrismaClient();
+
+export class ProfileService {
+  async getProfile(recipientDid: string) {
+    const profile = await prisma.privateProfile.findFirst({
+      where: {
+        authorDid: recipientDid,
+      },
+    });
+
+    if (!profile) {
+      throw new NotFoundError('Profile not found');
+    }
+
+    return profile;
+  }
+
+  async updateProfile(
+    authorDid: string,
+    profile: {
+      sessionId: string;
+      encryptedContent: string;
+      avatarUri?: string;
+      bannerUri?: string;
+    },
+  ) {
+    const existingProfile = await prisma.privateProfile.findFirst({
+      where: {
+        authorDid,
+      },
+    });
+
+    if (existingProfile) {
+      return await prisma.privateProfile.update({
+        where: {
+          id: existingProfile.id,
+        },
+        data: {
+          sessionId: profile.sessionId,
+          encryptedContent: Buffer.from(profile.encryptedContent),
+          avatarUri: profile.avatarUri,
+          bannerUri: profile.bannerUri,
+        },
+      });
+    } else {
+      return await prisma.privateProfile.create({
+        data: {
+          sessionId: profile.sessionId,
+          authorDid,
+          encryptedContent: Buffer.from(profile.encryptedContent),
+          avatarUri: profile.avatarUri,
+          bannerUri: profile.bannerUri,
+        },
+      });
+    }
+  }
+}
---
+++ services/private-profiles/src/views/private-sessions.views.ts
@@ -0,0 +1,35 @@
+import { Session, SessionKey } from '../generated/prisma-client/index.js';
+import {
+  createView,
+  createListView,
+  safeBtoa,
+} from '@speakeasy-services/common';
+
+export type EncryptedSessionKeyView = {
+  sessionId: string;
+  encryptedDek: string;
+  recipientDid: string;
+  createdAt: string;
+};
+
+/**
+ * Create a view that picks recipientDid and createdAt, converting createdAt to ISO string
+ */
+export const toSessionKeyView = createView<SessionKey, EncryptedSessionKeyView>(
+  ['sessionId', 'encryptedDek', 'recipientDid', 'createdAt'],
+  {
+    sessionId: (value: string) => value,
+    encryptedDek: (value: Uint8Array) => safeBtoa(value),
+    recipientDid: (value: string) => value,
+    userKeyPairId: (value: string) => value,
+    createdAt: (value: Date) => value.toISOString(),
+  },
+);
+
+/**
+ * Create a list view that maps over the array
+ */
+export const toSessionKeyListView = createListView<
+  SessionKey,
+  EncryptedSessionKeyView
+>(toSessionKeyView);
