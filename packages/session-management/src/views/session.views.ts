607c98fc88cf957e649c11f5857ec2c4f6a5f5a2
Chris Jensen <chris@speakeasy.local>
2025-06-26 17:18:12 +1200

    Simplify view and lexicon sharing

--- packages/session-management/src/views/session.views.ts
+++ packages/session-management/src/views/session.views.ts
@@ -1,4 +1,4 @@
-import { SessionKey } from '../session.service.js';
+import { SessionKeyModel } from '../session.service.js';
 
 import {
   createView,
@@ -16,21 +16,21 @@ export type EncryptedSessionKeyView = {
 /**
  * Create a view that picks recipientDid and createdAt, converting createdAt to ISO string
  */
-export const toSessionKeyView = createView<SessionKey, EncryptedSessionKeyView>(
-  ['sessionId', 'encryptedDek', 'recipientDid', 'createdAt'],
-  {
-    sessionId: (value: string) => value,
-    encryptedDek: (value: Uint8Array) => safeBtoa(value),
-    recipientDid: (value: string) => value,
-    userKeyPairId: (value: string) => value,
-    createdAt: (value: Date) => value.toISOString(),
-  },
-);
+export const toSessionKeyView = createView<
+  SessionKeyModel,
+  EncryptedSessionKeyView
+>(['sessionId', 'encryptedDek', 'recipientDid', 'createdAt'], {
+  sessionId: (value: string) => value,
+  encryptedDek: (value: Uint8Array) => safeBtoa(value),
+  recipientDid: (value: string) => value,
+  userKeyPairId: (value: string) => value,
+  createdAt: (value: Date) => value.toISOString(),
+});
 
 /**
  * Create a list view that maps over the array
  */
 export const toSessionKeyListView = createListView<
-  SessionKey,
+  SessionKeyModel,
   EncryptedSessionKeyView
 >(toSessionKeyView);
